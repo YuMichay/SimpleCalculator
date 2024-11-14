bfac9cce5288066c283b8df4615b7f7cb4e66601
Yuliya Kudrautsava <y.kudrautsava@andersenlab.com>
2024-11-14 18:49:11 +0100

    feat: added mode change, optimized html and css

--- src/script.js
+++ src/script.js
@@ -1,1 +1,19 @@
 import './style.css'
+
+// mode change
+let toMode = "dark";
+const modeButton = document.querySelector(".mode_toggle");
+
+const toggleMode = () => {
+  if (modeButton.textContent.startsWith('light')) {
+    toMode = 'dark';
+    document.body.classList.remove('dark-mode');
+    document.body.classList.add('light-mode');
+  } else {
+    toMode = 'light';
+    document.body.classList.remove('light-mode');
+    document.body.classList.add('dark-mode');
+  }
+  modeButton.textContent = toMode + ' mode';
+}
+modeButton.addEventListener('click', toggleMode);
